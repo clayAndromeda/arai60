44910c78a4f2a8a9e97ee92d913fc3b9520d92d8
clayAndromeda <andromeda4869game@gmail.com>
2026-06-27 20:04:38 +0900

    up

--- LInkedList/LinkedListCycle/Main.cs
+++ LInkedList/LinkedListCycle/Main.cs
@@ -1,25 +1,26 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     public int val;
- *     public ListNode next;
- *     public ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int x)
+    {
+        val = x;
+        next = null;
+    }
+}
+
 public class Solution {
     public bool HasCycle(ListNode head) {
         HashSet<int> valueSet = new();
-        while (head != null)
+        ListNode current = head;
+        while (current != null)
         {
-            if (valueSet.Contains(head.val))
+            if (valueSet.Contains(current.val))
             {
                 return true;
             }
-            valueSet.Add(head.val);
-            head = head.next;
+            valueSet.Add(current.val);
+            current = current.next;
         }
         return false;
     }
