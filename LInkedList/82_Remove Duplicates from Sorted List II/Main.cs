4236931d0f01ed420e150ad6316bd965244b52a1
clayAndromeda <andromeda4869game@gmail.com>
2026-06-29 00:47:31 +0900

    Up

---
+++ LInkedList/82_Remove Duplicates from Sorted List II/Main.cs
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public ListNode DeleteDuplicates(ListNode head)
+    {
+        var dummyHead = new ListNode(-1, head);
+        var prev = dummyHead;
+        var current = head;
+
+        while (current != null)
+        {
+            if (current.next != null && current.val == current.next.val)
+            {
+                int valToDelete = current.val;
+                while (current != null && current.val == valToDelete)
+                {
+                    current = current.next;
+                }
+                prev.next = current;
+            }
+            else
+            {
+                prev = current;
+                current = current.next;
+            }
+        }
+
+        return dummyHead.next;
+    }
+}
