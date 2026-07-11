ffb91aacb4bb089f9fd8e7220bf03291bdf132aa
clayAndromeda <andromeda4869game@gmail.com>
2026-07-12 00:50:46 +0900

    up

--- Heap/347_Top K Frequent Elements/Main.cs
+++ Heap/347_Top K Frequent Elements/Main.cs
@@ -1,7 +1,26 @@
+using System.Runtime.InteropServices;
+
 public class Solution
 {
     public int[] TopKFrequent(int[] nums, int k)
     {
+        Dictionary<int, int> dict = new();
+        foreach (int num in nums)
+        {
+            ref int freq = ref CollectionsMarshal.GetValueRefOrAddDefault(dict, num, out _);
+            freq++;
+        }
 
+        PriorityQueue<int, int> priorityQueue = new(k + 1);
+        foreach (var (num, freq) in dict)
+        {
+            priorityQueue.Enqueue(num, freq);
+            if (priorityQueue.Count > k)
+            {
+                priorityQueue.Dequeue();
+            }
+        }
+
+        return priorityQueue.UnorderedItems.Select(x => x.Element).ToArray();
     }
 }
