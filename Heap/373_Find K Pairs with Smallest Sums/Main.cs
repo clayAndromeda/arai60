b0f3ed4295a6acfd5930d227991445e69f5412e5
clayAndromeda <andromeda4869game@gmail.com>
2026-07-15 00:25:52 +0900

    up

--- Heap/373_Find K Pairs with Smallest Sums/Main.cs
+++ Heap/373_Find K Pairs with Smallest Sums/Main.cs
@@ -2,25 +2,22 @@ public class Solution
 {
     public IList<IList<int>> KSmallestPairs(int[] nums1, int[] nums2, int k)
     {
-        PriorityQueue<(int i, int j), int> queue = new();
-        for (int i = 0; i < Math.Min(nums1.Length, k); i++)
+        PriorityQueue<(int U, int V), int> pQueue = new(k + 1);
+        for (int u = 0; u < nums1.Length; ++u)
         {
-            queue.Enqueue((i, 0), nums1[i] + nums2[0]);
+            pQueue.Enqueue((U: u, V: 0), nums1[u] + nums2[0]);
         }
 
         List<IList<int>> result = new();
-
-        while (result.Count < k && queue.Count > 0)
+        while (result.Count < k && pQueue.Count > 0)
         {
-            var (i, j) = queue.Dequeue();
-            result.Add(new List<int> { nums1[i], nums2[j] });
-
-            if (j + 1 < nums2.Length)
+            (int u, int v) = pQueue.Dequeue();
+            result.Add(new List<int>() { nums1[u], nums2[v] });
+            if (v + 1 < nums2.Length)
             {
-                queue.Enqueue((i, j + 1), nums1[i] + nums2[j + 1]);
+                pQueue.Enqueue((U: u, V: v + 1), nums1[u] + nums2[v + 1]);
             }
         }
-
         return result;
     }
 }
