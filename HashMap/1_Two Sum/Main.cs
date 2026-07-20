4b2f3bd3d6663c65cace5fe86080f6329ab970b9
clayAndromeda <andromeda4869game@gmail.com>
2026-07-21 08:17:13 +0900

    up

--- HashMap/1_Two Sum/Main.cs
+++ HashMap/1_Two Sum/Main.cs
@@ -1,23 +1,18 @@
 public class Solution
 {
-    // numsをDictionaryでアクセスできるようにする
-    // nums[i]と足し合わせてtargetになる値がnumsにあるかを調べる
-    // -> これだと、numsに同じ値があるときに困る。Dictionary<int, int[]> だとまあ対応できるが。。。
-    // いや、Dictionaryを構築しながら見ていけばいいのか。
-
     public int[] TwoSum(int[] nums, int target)
     {
-        Dictionary<int, int> numToIndex = new(nums.Length);
-        for (int i = 0; i < nums.Length; ++i)
+        Dictionary<int, int> keyToIndex = new();
+        for (int index = 0; index < nums.Length; ++index)
         {
-            if (numToIndex.TryGetValue(target - nums[i], out var pairIndex))
+            if (!keyToIndex.TryGetValue(target - nums[index], out var pairIndex))
             {
-                return new int[] { pairIndex, i };
+                keyToIndex[nums[index]] = index;
+                continue;
             }
-
-            numToIndex[nums[i]] = i;
+            return new int[] { index, pairIndex };
         }
 
-        throw new ArgumentException("No two numbers add up to the target.");
+        throw new ArgumentException("The Answer not found");
     }
 }
